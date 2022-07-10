e1b8f72b04cd45213e0717fa2d5566855a1434e5
altersaber26 <altersaber26@gmail.com>
2022-07-10 18:18:56 +0300

    Added separate main.py file to handle initializing

---
+++ main.py
@@ -0,0 +1,44 @@
+# Environment variables and threading for terminal loop
+from dotenv import load_dotenv
+import os
+import threading
+
+# Telegram stuff
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# My Modules
+import controller
+import cli
+
+
+load_dotenv()
+
+ # Initializing the Bot
+updater = Updater(token=os.getenv("TELEGRAM_BOT_TOKEN"))
+dp = updater.dispatcher
+
+# Custom Filter to filter down any requests from all users except me (my User ID)
+correct_user_filter = Filters.user(user_id=int(os.getenv("TELEGRAM_USER_ID")))
+
+# All handlers
+dp.add_handler(CommandHandler("start", controller.start, filters=correct_user_filter))
+dp.add_handler(CommandHandler("help", controller.help, filters=correct_user_filter))
+dp.add_handler(CommandHandler("expense", controller.expense, filters=correct_user_filter))
+dp.add_handler(CommandHandler("income", controller.income, filters=correct_user_filter))
+dp.add_handler(CommandHandler("balance", controller.balance_query, filters=correct_user_filter))
+dp.add_handler(CommandHandler("convert", controller.convert, filters=correct_user_filter))
+dp.add_handler(CommandHandler("month", controller.month_query, filters=correct_user_filter))
+dp.add_handler(CommandHandler("del", controller.delete_expense, filters=correct_user_filter))
+dp.add_handler(MessageHandler(Filters.text & correct_user_filter, controller.handle_message))
+
+print("Bot running...")
+
+# Creating a separate thread for recieving terminal commands
+terminal_thread = threading.Thread(target=cli.terminal_loop)
+terminal_thread.start()
+
+# Starting connection to Telegram servers
+updater.start_polling(poll_interval=1, timeout=5)
+
+# Block main thread from dying
+updater.idle()
